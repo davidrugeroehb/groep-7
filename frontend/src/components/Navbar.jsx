90ccf8a5b8aabebf2da002c36779626f98809634
frs-bgh <fariss.boughaba@student.ehb.be>
2025-06-04 13:59:42 +0200

    jhszhks

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/ehb.jpg'; 
+import logo from '../assets/ehb.jpg';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-         <h1>Test Navbar</h1>
-      <img src={logo} alt="logo" />
-      <ul>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/login">Login</NavLink></li>
-        <li><NavLink to="/mijnaanvragen">Mijn Aanvragen</NavLink></li>
-        <li><NavLink to="/mijnafspraken">Mijn Afspraken</NavLink></li>
-        <li><NavLink to="/mijnprofiel">Mijn Profiel</NavLink></li>
-        <li><NavLink to="/speeddates">Speeddates</NavLink></li>
+    <nav className="bg-white shadow-md p-4 flex items-center justify-between">
+      {/* Logo */}
+      <div className="flex items-center space-x-4">
+        <img src={logo} alt="logo" className="h-12 w-auto" />
+        <h1 className="text-xl font-bold">Groep 7</h1>
+      </div>
+
+      {/* Links */}
+      <ul className="flex space-x-6 text-sm font-medium">
+        <li><NavLink to="/about" className="hover:text-blue-600">About</NavLink></li>
+        <li><NavLink to="/login" className="hover:text-blue-600">Login</NavLink></li>
+        <li><NavLink to="/mijnaanvragen" className="hover:text-blue-600">Mijn Aanvragen</NavLink></li>
+        <li><NavLink to="/mijnafspraken" className="hover:text-blue-600">Mijn Afspraken</NavLink></li>
+        <li><NavLink to="/mijnprofiel" className="hover:text-blue-600">Mijn Profiel</NavLink></li>
+        <li><NavLink to="/speeddates" className="hover:text-blue-600">Speeddates</NavLink></li>
       </ul>
-    </div>
+    </nav>
   );
 };
 
