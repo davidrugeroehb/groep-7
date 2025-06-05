b1496ca78a85a633d6634b9e15ec92abc35145a8
frs-bgh <fariss.boughaba@student.ehb.be>
2025-06-05 10:30:35 +0200

    navbar gewijzigd, titel link naar homepage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/ehb.jpg';
@@ -13,7 +14,10 @@ const Navbar = () => {
       {/* Logo + Titel links */}
       <div className="flex items-center space-x-3">
         <img src={logo} alt="logo" className="h-10 w-auto" />
-        <h1 className="text-xl font-bold">Career Match</h1>
+        <Link to="/speeddates" className="text-xl font-bold">
+  Career Match
+        </Link>
+
       </div>
 
       {/* alle navlinks in het midden zetten */}
