bbe8e490ff0f097e86f672aca34738f8f08dbba7
lbscv <lars.biczysko@student.ehb.be>
2025-06-10 10:47:03 +0200

    commit

--- frontend/src/pages/Bedrijf/Mail_intre.jsx
+++ frontend/src/pages/Bedrijf/Mail_intre.jsx
@@ -3,7 +3,16 @@ import React, { useEffect, useState } from "react";
 function MailIntresse() {
 
   return (
-    <div></div>
+     
+    <div>
+        <form>
+            <p>Mailadres</p>
+            <input type="email" required></input>
+            <p>inhoud</p>
+            <textarea id="inhoud" name="mail" rows="10" cols="50" required></textarea>
+            <button type="submit">Verzend</button>
+        </form>
+    </div>
   );
 }
 
