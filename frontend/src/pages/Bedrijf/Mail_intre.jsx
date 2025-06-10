94d0b492a5dd186b5166f8b699a4f82012e3d143
lbscv <lars.biczysko@student.ehb.be>
2025-06-10 10:55:35 +0200

    commit

--- frontend/src/pages/Bedrijf/Mail_intre.jsx
+++ frontend/src/pages/Bedrijf/Mail_intre.jsx
@@ -4,13 +4,13 @@ function MailIntresse() {
 
   return (
      
-    <div>
-        <form>
-            <p>Mailadres</p>
-            <input type="email" required></input>
-            <p>inhoud</p>
-            <textarea id="inhoud" name="mail" rows="10" cols="50" required></textarea>
-            <button type="submit">Verzend</button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <form className="bg-white p-8 rounded shadow-md w-full max-w-md">
+            <p>Mailadres</p> <br></br>
+            <input type="email" required></input><br></br>
+            <p>inhoud</p><br></br>
+            <textarea id="inhoud" name="mail" rows="10" cols="50" required></textarea><br></br>
+            <button type="submit" className="bg-blue-500 text-white w-full py-2 rounded-md text-base hover:bg-blue-600 transition">Verzend</button>
         </form>
     </div>
   );
