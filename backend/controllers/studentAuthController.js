197625657adaac3560c7de76fb0a0a2c20386e94
lbscv <lars.biczysko@student.ehb.be>
2025-06-10 15:00:07 +0200

    Commit

--- backend/controllers/studentAuthController.js
+++
@@ -1,6 +0,0 @@
-import studentModel from "../models/studentModel.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-
-// Enkel login voor studenten (REMOVED - now handled by authController.js)
-// export const loginStudent = async (req, res) => { /* ... */ };
