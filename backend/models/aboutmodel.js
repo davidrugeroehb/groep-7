cd383b0f872ca0b616ccd7cd6014a3589b35704f
lbscv <lars.biczysko@student.ehb.be>
2025-06-12 14:13:05 +0200

    fout verbetert in aboutmodel
    Deze model gaf een foutmelding af.

--- backend/models/aboutmodel.js
+++ backend/models/aboutmodel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const aboutschema=new mongoose.Schema({
-    tekst_about: {String: true}
+    tekst_about: {String: true, required: true}
 });
 const About=mongoose.model('About', aboutschema);
 export default About
