62c3cd2ea783f3e9644bfd77e4d3d4faf41dee3f
lbscv <lars.biczysko@student.ehb.be>
2025-06-18 09:48:46 +0200

    Basis pagina
    De basis pagina is nu deftig afgemaakt

--- frontend/src/pages/Basis.jsx
+++ frontend/src/pages/Basis.jsx
@@ -14,9 +14,19 @@ function Basis() {
                 We verbinden studenten en bedrijven op een vlotte en efficiënte manier via speeddates.
             </p>
             <hr/>
-            <p>Lars is de beste persoon</p>
+            <br/>
+            <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Wat is Career Launch</h2>
+            <p className="text-gray-700 mb-6 leading-relaxed">Career Launch is de springplank van talent naar toekomst. Het is een intensief en doelgericht traject waarin je niet alleen ontdekt wie je bent en wat je kunt, maar ook leert hoe je dat overtuigend kunt inzetten in de praktijk.</p>
+            <hr/>
+            <br/>
+            <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Applicatie gemaakt door</h2>
+            <p className="text-gray-700 mb-6 leading-relaxed">Deze applicatie werd ontwikkeld door de getalenteerde studenten Toegepaste Informatica aan de Erasmushogeschool Brussel (EhB): Lars Biczysko, Brend De Greef, Wiam Bola, Nehad Tabbakhe, David Rugero en Fariss Boughaba.
+              De ontwikkeling van deze applicatie gebeurde in opdracht van de Erasmushogeschool Brussel.
+            </p>
         </div>
-        <img src={basis} alt="foto van studenten van ehb die praten met een verandwoordelijke van een bedrijf" className="mt-8 max-w-full w-full md:w-2/3 rounded-xl shadow-md object-cover" />
+         <div className="flex justify-center mt-8">
+          <img src={basis} alt="foto van studenten van ehb die praten met een verandwoordelijke van een bedrijf" className="mt-8 max-w-full w-full md:w-2/3 rounded-xl shadow-md object-cover" />
+        </div>
     </div>
   );
 }
